56615c50c208cd3bf0e6d0519758b12b421bf7df
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-04 15:36:47 +0530

    update

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  './favicon.ico'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="main">
+      <div className='first_div'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque obcaecati quas molestiae nemo fugiat in est reiciendis incidunt inventore minus voluptates magni dignissimos architecto cumque, porro optio sint. Tenetur!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque obcaecati quas molestiae nemo fugiat in est reiciendis incidunt inventore minus voluptates magni dignissimos architecto cumque, porro optio sint. Tenetur!</p>
+    </div>
+    <div className='second_div'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque obcaecati quas molestiae nemo fugiat in est reiciendis incidunt inventore minus voluptates magni dignissimos architecto cumque, porro optio sint. Tenetur!</p>
+    </div>
+    <div className='third_div'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque obcaecati quas molestiae nemo fugiat in est reiciendis incidunt inventore minus voluptates magni dignissimos architecto cumque, porro optio sint. Tenetur!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque obcaecati quas molestiae nemo fugiat in est reiciendis incidunt inventore minus voluptates magni dignissimos architecto cumque, porro optio sint. Tenetur!</p>
+    </div>
+    </div>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
