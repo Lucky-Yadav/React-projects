86c34e1fabfdd56948cb472b3f336b1aaa08e972
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-13 15:31:42 +0530

    Update GroceryList.jsx

--- grocery/src/components/grocery/GroceryList.jsx
+++ grocery/src/components/grocery/GroceryList.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 const Todolist = ({ data }) => {
-  const deletedata = (id) => {
+  function deletedata  (key) {
     
+    for (let index = 0; index < data.length; index++) {
+      console.log(key);
+      if (data[index]["id"] === key) {
+        delete data[index]
+        // 
+      }
+    } Todolist({data})
+    console.log(data);
   };
   return (
     <>
       <h2>list</h2>
       {data.map((item) => (
         <>
-          <h1 onClick={deletedata(data.id)} key={item.id}>
+          <h1
+            key={item.id}
+            onClick={(e) => {
+              let key = item.id
+              deletedata(key);
+            }}
+          >
             {item.title}
           </h1>
         </>
       ))}
-      <div>{console.log(data)}</div>
+      {/* <div>{console.log(data)}</div> */}
     </>
   );
 };
-export default Todolist;
+export default Todolist
