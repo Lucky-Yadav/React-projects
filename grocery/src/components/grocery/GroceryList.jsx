2caeb55a4c3e1f877f6b2fae85121152bdf06f89
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-13 11:57:06 +0530

    change

--- grocery/src/components/grocery/GroceryList.jsx
+++ grocery/src/components/grocery/GroceryList.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 const Todolist = ({ data }) => {
+  const deletedata = (id) => {
+    
+  };
   return (
     <>
       <h2>list</h2>
       {data.map((item) => (
-        <h1 onClick={deletedata(data.id)} key={item.id}>{item.title}</h1>
+        <>
+          <h1 onClick={deletedata(data.id)} key={item.id}>
+            {item.title}
+          </h1>
+        </>
       ))}
-      <div>{ console.log(data)}</div>
+      <div>{console.log(data)}</div>
     </>
   );
 };
--- grocery/src/components/grocery/grocery.jsx
+++ grocery/src/components/grocery/grocery.jsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const Todo = () => {
   const [data, setdata] = React.useState([]);
+  
   const handleadditem = (title) => {
     const todoitem = {
       title: title,
