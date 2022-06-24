2a0b12dc88ebe7af8035e779ce021ed8d08326da
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-06-25 00:08:08 +0530

    Update logout.jsx

--- Redux/first-react-redux/src/components/logout.jsx
+++ Redux/first-react-redux/src/components/logout.jsx
@@ -1,3 +1,10 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../App";
+
+
+const logout = () => {
+    
+}
+
+export default logout
