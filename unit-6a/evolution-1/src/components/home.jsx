dbb6555824d5d0eb612adf460a4178d5022d0d07
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-21 17:22:29 +0530

    Update home.jsx

--- unit-6a/evolution-1/src/components/home.jsx
+++ unit-6a/evolution-1/src/components/home.jsx
@@ -4,23 +4,11 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [value, setvalue] = React.useState(0);
-  const [items, setitem] = React.useState([]);
-  const handlecart = (a) => {
-    console.log("clicked");
-    setvalue(value + a);
-    };
-    const navigate = useNavigate()
-
-  React.useEffect(() => {
-    fetch(`http://localhost:8080/movies`)
-      .then((res) => res.json())
-      .then((res) => setitem(res));
-  }, []);
 
   return (
     <div>
-      <h1>Appointments page</h1>
-      <div> cart {value}</div>
+          <h1>Appointments page</h1>
+          <button className="delete" >go to Appointments page</button>
       
     </div>
   );
