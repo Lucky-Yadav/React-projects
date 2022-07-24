da99061014dcf1cef1418e76b2cf2ac4765e0040
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-24 10:31:16 +0530

    update

--- assignment-4/src/context/authcontext.jsx
+++ assignment-4/src/context/authcontext.jsx
@@ -1,3 +1,7 @@
 import React, { createContext } from "react"
 
 export const Authcontext = createContext()
+
+export const AuthProvider = ({ children }) => {
+    return <Authcontext.Provider>{ children }</Authcontext.Provider>
+}
