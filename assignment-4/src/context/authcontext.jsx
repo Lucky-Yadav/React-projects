c7055bd93c12a5ec2ece5582a5b84dc4abd4f99f
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-25 19:14:46 +0530

    update authcontext

--- assignment-4/src/context/authcontext.jsx
+++ assignment-4/src/context/authcontext.jsx
@@ -1,7 +1,38 @@
-import React, { createContext } from "react"
+import axios from "axios";
+import React, { createContext, useState } from "react"
 
 export const Authcontext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    return <Authcontext.Provider>{children}</Authcontext.Provider>
+     const [token, setToken] = useState(null)
+
+     const handlelogin = (body) => {
+       const payload = { email, password };
+       console.log(payload);
+         axios({
+           method: "POST",
+             url: "https://reqres.in/api/login",
+             data: body
+         })
+           .then((res) => res.json())
+           .then((res) => {
+             console.log(res);
+             if (res.token) {
+               alert("Successfuly logged in");
+
+               navigate("/");
+               // navigate("/booking")
+             }
+           })
+           .catch((err) => {
+             console.log(err);
+           });
+     };
+
+    const value = {
+        handlelogin,
+        token
+     }
+
+    return <Authcontext.Provider value={value}>{children}</Authcontext.Provider>
 };
