371a017fccf18f19bf4ca014b3cfa8a6c3428a58
Luckyyadav420 <97690042+LuckyYadav786@users.noreply.github.com>
2022-07-25 19:17:01 +0530

    Update navbar.jsx

--- assignment-4/src/components/navbar.jsx
+++ assignment-4/src/components/navbar.jsx
@@ -11,6 +11,7 @@ const NavbarWrapper = styled.div`
 `;
 
 const Navbar = () => {
+    const {token, handlelogin} = useContext(second)
     return (
         <NavbarWrapper>
             <Link to="/"> home </Link>
