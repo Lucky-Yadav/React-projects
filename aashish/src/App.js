91ca45c2de417e322118d59a6c68d7b66cba2a9a
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-10-05 07:22:10 +0530

    update app

---
+++ aashish/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      aashiash
+      <h1>aashiash</h1>
+    </div>
+  );
+}
+
+export default App;
--- demo/src/App.js
+++ demo/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from 'axios'
 
+const APIURL = "https://jsonplaceholder.typicode.com/users";
 function App() {
+  const [data, setdata] = useState([])
+  const [byname, setbyname] = useState([]);
+  const getUsers = () => {
+    axios
+      .get(APIURL)
+      .then((res) => {
+        console.log(res.data);
+        setdata  (res.data)
+        console.log(data);
+      })
+      .then((err) => {
+        console.log(err);
+      });
+    //write code here
+    //render the list of users
+  };
+  var count = 1
+  const sortList = () => {
+    if (count == 1) {
+
+    }
+    //write code here
+    // sort the user list by name's length
+    // on first click list will sorted in assending order
+    // on second click list will be sorted in descending order
+    // on third click default list will be rendered
+    // on fourth click again start form step 1
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>User List</h1>
+      <div>
+        <button onClick={getUsers}>Get Users</button>
+        <button onClick={sortList}>Sort list by name's length</button>
+      </div>
+      <ul>{/*render the list of the users */}</ul>
+      {data.map(user => (
+        <div className="users" key={user.id}>
+          <div className="user">name= {user.name} id= { user.id}  </div>
+        </div>
+      ) ) }
+    </main>
   );
 }
 
